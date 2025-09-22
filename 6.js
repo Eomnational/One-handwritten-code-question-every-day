9bd3f658ce394d4611de31b7fd927ec0c6c60a7e
Zero1017 <109895777+Eomnational@users.noreply.github.com>
2025-09-22 13:30:27 +0800

    feat: 数组拍平

---
+++ 6.js
@@ -0,0 +1,21 @@
+function test1(arr){
+    return arr.flat(Infinity);
+}
+
+let arr=[1,[2,3],[4,[5,6]]];
+
+
+function test2(arr){
+    let res=[];
+    arr.forEach((i) => {
+        if(i instanceof Array){
+            res=res.concat(test2(i));
+        }else{
+            res.push(i);
+        }
+    });
+
+    return res;
+}
+
+console.log(test2(arr));
