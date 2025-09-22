70fa7d8ee499bd49aca4a07a25c245e65e0826e5
Zero1017 <109895777+Eomnational@users.noreply.github.com>
2025-09-22 09:29:16 +0800

    feat: 1.防抖

--- 1.js
+++ 1.js
@@ -1,5 +1,26 @@
-function create(obj){
-    function F(){};
-    F.prototype = obj;
-    return new F();
+function debounce(func,delay){
+    let timer=null;
+    return function(...args){
+        clearTimeout(timer);
+        timer=setTimeout(()=>{
+            func.call(this,args);
+        },delay);
+    }
+    
 }
+
+
+// 原始函数
+function sayHello(name) {
+  console.log(`Hello, ${name}！`);
+}
+
+// 防抖处理，延迟 1000ms
+const debouncedSayHello = debounce(sayHello, 1000);
+
+// 快速多次调用
+debouncedSayHello('Alice'); // 不会立即执行
+debouncedSayHello('Bob');   // 会重置计时
+debouncedSayHello('Tom');   // 会重置计时
+
+// 只有等你停止调用 1 秒后，才会输出：Hello, Tom！
